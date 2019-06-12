ab16b895e20c1d5daf620d623ed29e0e40155bb9
cc <1073792244@qq.com>
2019-06-12 16:58:33 +0800

    mlj_c

--- Master/src/Master/Test.java
+++ Master/src/Master/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("as");
-		System.out.println("hello21212");
+		System.out.println("hello21212_mlj");
 		System.out.println("wyl“ŸˆêŸà");
 	
 }
