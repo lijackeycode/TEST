a1c175edbbba233b367f8a74f4c465be5fa8d77e
刘海涛 <329239539@qq.com>
2019-06-12 15:27:07 +0800

    上传代码

--- git/src/dome/Test.java
+++ git/src/dome/Test.java
@@ -1,10 +1,21 @@
 package dome;
 
+import java.util.Scanner;
+
 public class Test {
 
-	
+	 static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		System.out.println("hello word");
+		System.out.println("ÇëÊäÈëÊı×Ö");
+		 int con = scanner.nextInt();
+		 if(con == 666) {
+			 
+			 System.out.println("É·±Ê");
+			 
+		 }else {
+			System.out.println("Á÷Æ¤");
+		}
+		
 	}
 }
